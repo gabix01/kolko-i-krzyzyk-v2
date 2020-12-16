41ec96e4de03ae073e0baea8377d7be813b02b3c
konrad <konradmielnik@gmail.com>
2020-12-16 03:16:33 +0100

    ciekawe

---
+++ Assets/restar buttom.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+[RequireComponent(typeof(Button))]
+public class Reset : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
